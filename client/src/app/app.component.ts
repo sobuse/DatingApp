025457b2967bf532fd0794510da71a006586d198
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-09-09 19:33:01 +0100

    configur get Users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,22 +4,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'The dating App';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
+  
   ngOnInit() {
     this.getUsers();
   }
+
   getUsers() {
-    this.http.get('http://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-    
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (response) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
