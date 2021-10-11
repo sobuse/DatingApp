77112aaccf210f9f4e64571031990ff8cd629a2e
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-10-11 15:25:07 +0100

    created Token

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -21,7 +23,7 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-            
+
         }
 
         public IConfiguration Configuration { get; }
@@ -29,10 +31,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddCors();
 
             services.AddControllers();
-            
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<Data.DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
@@ -55,11 +58,11 @@ namespace API
             }
 
             app.UseHttpsRedirection();
-         
+
 
             app.UseRouting();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
-           
+
 
             app.UseAuthorization();
 
