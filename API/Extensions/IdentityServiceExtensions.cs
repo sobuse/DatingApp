f60be03cb0e7bd42b96e0b63c5b0f269f1210ca5
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-10-28 12:02:59 +0100

    Adding Extension method

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection)
+    }
+}
